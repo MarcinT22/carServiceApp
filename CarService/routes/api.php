6b1b0232cffe4bd62bda422f841df65267fe1a41
Marcin Topolski <marcin.topolski96@gmail.com>
2020-04-28 16:24:28 +0200

    passport auth

--- CarService/routes/api.php
+++ CarService/routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('login', 'Api\UserApiController@login');
+Route::post('register', 'Api\UserApiController@register');
 
 Route::get('cars','Api\CarApiController@list');
 Route::post('cars','Api\CarApiController@store');
