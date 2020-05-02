0cc8c34286e2104702e13f2a2c73f9b2e00f98a3
Marcin Topolski <marcin.topolski96@gmail.com>
2020-05-02 20:26:50 +0200

    mobile app

---
+++ mobileApp/src/App.vue
@@ -0,0 +1,97 @@
+<template>
+    <div id="app">
+        <header>
+            <img src="./assets/image/logo.png"/>
+        </header>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import VueTouchRipple from 'vue-touch-ripple'
+    import 'vue-touch-ripple/dist/vue-touch-ripple.css'
+
+    Vue.use(VueTouchRipple, {
+        // default global options
+        opacity: 0.2,
+        speed: 2,
+        color: '#f7f7f7'
+    })
+
+    export default {
+        name: 'App',
+        data() {
+            return {
+                isClicked: false
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css');
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap');
+    @import "assets/scss/config";
+
+    html, body {
+        padding: 0;
+        margin: 0;
+        font-family: 'Open Sans', sans-serif;
+        font-weight: 400;
+        background: #f7f7f7;
+        position: relative;
+        height: 100%;
+
+        &::before {
+            content: '';
+            display: block;
+            height: 50vw;
+            width: 100%;
+            position: fixed;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background: #fff;
+            transform: skewY(-25deg);
+            top: -25vw;
+            z-index: 0;
+        }
+
+        &::after {
+            content: url('assets/image/background_3.jpg');
+            display: block;
+            position: fixed;
+            bottom: -25px;
+            right: -10px;
+            z-index: 0;
+            transform: scale(0.9);
+            opacity: 0.075;
+        }
+    }
+
+    #app {
+        position: relative;
+        z-index: 1;
+        padding: 0 20px;
+    }
+
+    a {
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        -webkit-tap-highlight-color: transparent;
+    }
+
+    header {
+        text-align: center;
+        padding: 40px;
+
+        img {
+            display: block;
+            margin: 0 auto;
+            max-width: 100px;
+            opacity: 0.5;
+        }
+    }
+
+</style>
---
+++ mobileApp/src/components/Home.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="home">
+        <div class="home__text">
+            <i class="far fa-calendar-alt"></i>
+            Umów wizytę
+        </div>
+        <div class="home__buttons">
+            <touch-ripple class="home__button">
+                <a href="#">
+                    <i class="fas fa-car"></i>
+                    Mój samochód
+                </a>
+            </touch-ripple>
+            <touch-ripple class="home__button">
+                <a href="#">
+                    <i class="fas fa-plus-circle"></i>
+                    Nowy samochód
+                </a>
+            </touch-ripple>
+        </div>
+        <div class="home__text">
+            lub
+        </div>
+        <div class="home__buttons">
+            <touch-ripple class="home__button home__button--large">
+                <a href="#">
+                    <i class="far fa-check-circle"></i>
+                    Sprawdź status
+                </a>
+            </touch-ripple>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Home",
+
+    }
+</script>
+
+<style scoped lang="scss">
+    @import "../assets/scss/config";
+
+    .home {
+        &__text {
+            color: $mainColor;
+            font-size: 18px;
+            text-align: center;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+
+            i {
+                font-size: 25px;
+                margin-bottom: 5px;
+            }
+        }
+
+        &__buttons {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding:15px 0px;
+        }
+
+        &__button{
+            padding:0 25px;
+
+            a{
+                display: flex;
+                flex-direction: column;
+                font-size:13px;
+                text-decoration: none;
+                justify-content: center;
+                align-items: center;
+                color:#fff;
+                border-radius: 100%;
+                background:url('../assets/image/background_2.jpg')no-repeat;
+                background-size: cover;
+                width:120px;
+                height:120px;
+                i{
+                    font-size:24px;
+                    margin-bottom:5px;
+                }
+            }
+
+            &--large{
+                width: 100%;
+                max-width:290px;
+                margin-top:10px;
+                padding-left:0;
+                padding-right:0;
+                a{
+                    border-radius: 15px;
+                    width: 100%;
+                    height:auto;
+                    padding:15px 10px;
+                    box-sizing: border-box;
+                    font-size:16px;
+                    background:url('../assets/image/background_1.jpg')no-repeat;
+                    background-position: right center;
+                    background-size: cover;
+
+                }
+            }
+        }
+    }
+</style>
